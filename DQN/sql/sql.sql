7ca12260163d43f346bec9baf587fb726c3d639e
Divya Nayan <divyanayan88@gmail.com>
2025-02-04 15:49:31 +0530

    Update sql.sql

--- DQN/sql/sql.sql
+++ DQN/sql/sql.sql
@@ -1,7 +1,3 @@
-SELECT *, 
-       DATEFROMPARTS(year, month, day) AS TrxDate
-FROM [Yaumi_Forecast_RouteItem_2025-01-10_2Routes_updated_3003_3004]
-WHERE CustomerCode IN ('3003', '3004') 
-AND ItemCode IN ('50-4072', '50-0261', '50-4085', '50-0276', '50-0102', 
-                 '50-0117', '50-0526', '50-0401', '50-0412', '50-0296')
-ORDER BY TrxDate;
+#provide sql query if you wish to fetch data for further analysis or training.
+       
+SELECT whatever you want to have for your purpose from your table;
